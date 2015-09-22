6e1a548c23a9b6944455d8b114c090be2842b1fa
David Voong <voong.david@gmail.com>
2015-09-22 21:57:45 +0000

    Modify server to save data into a rotating collection and rotating log file

--- scripts/start_server.py
+++ scripts/start_server.py
@@ -3,6 +3,8 @@ import argparse
 import json
 import logging
 import datetime
+import pymongo
+import logging.handlers
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 from pymongo import MongoClient
@@ -11,51 +13,37 @@ logger = logging.getLogger(__name__)
 
 class Echo(DatagramProtocol):
 
-    def __init__(self, log_file=None, *args, **kwargs):
-        self.log_file = log_file
-        print self.log_file
-        # DatagramProtocol.__init__(self, *args, **kwargs)
-        # super(Echo, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        self.client = MongoClient()
+        self.db = self.client.database
+        try:
+            self.collection = self.db.create_collection('collection', capped=True, size=100)
+            print 'creating new collection'
+        except pymongo.errors.CollectionInvalid:
+            self.collection = self.db.collection
 
     def datagramReceived(self, data, (host, port)):
-        try:
-            json_ = json.loads(data)
-            client = MongoClient()
-            logging.info('MongoDB client: {}'.format(client))
-            logging.info('Creating/getting collection: tfl_data')
-            db = client.tfl_data
-            logging.info('db: {}'.format(db))
-            logging.info('Creating/getting my_collection')
-            collection = db.my_collection
-            logging.info('my_collection: {}'.format(collection))
-            logging.info('inserting entry into collection:')
-            json_['datetime'] = datetime.datetime.now()
-            result = collection.insert_one(json_)
-            logging.info('returned output: {}'.format(result))
-            logging.info('collection contents:')
-            cursor = collection.find()
-            for document in cursor:
-                logging.info(document)
-            
-        except ValueError as e:
-            print "received %r from %s:%d" % (data, host, port)
-
-        if self.log_file:
-            self.log_file.write('{datetime}: {data}\n'.format(datetime=datetime.datetime.now(), data=data))
+        document = {}
+        document['datetime'] = datetime.datetime.now()
+        document['msg'] = data
+        result = self.collection.insert_one(document)
+        logger.info(data)
         
         # self.transport.write(data, (host, port))
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
-
     parser = argparse.ArgumentParser(description="Starts a basic UDP server.")
     parser.add_argument('--port', type=int, default=9999)
     parser.add_argument('--log-file', default='log.log')
     args = parser.parse_args()
 
-    print args
+    print 'args:', args
 
-    with open(args.log_file, 'wb', 0) as f:
+    handler = logging.handlers.RotatingFileHandler(args.log_file, maxBytes=1e6, backupCount=3)
+    formatter = logging.Formatter("%(asctime)s: %(message)s", '%Y-%m-%d %H:%M:%S')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
 
-        reactor.listenUDP(9999, Echo(f))
-        reactor.run()
+    reactor.listenUDP(9999, Echo())
+    reactor.run()
